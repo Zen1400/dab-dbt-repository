9844fa7dada2478509cc1644594ec97d948c13c6
Zein Moualla <zeinda1458@gmail.com>
2023-11-01 05:49:41 +0000

    d

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -2,21 +2,21 @@ with
 
 source as (
 
-    select * from {{ source('raw', 'sales') }}
+    select  *   from {{ source('raw', 'sales') }}
 
 ),
 
 renamed as (
 
-    select
-        date_date,
+    select  date_date,
         orders_id,
-        pdt_id as 'products_id'
+        pdt_id as products_id,
         revenue,
         quantity
+
 
     from source
 
 )
 
-select * from renamed
+select *, concat(orders_id, '-', products_id) as pk from renamed
